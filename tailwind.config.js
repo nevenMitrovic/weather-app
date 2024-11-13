2540e70dfb9187388782946252bb3a2316de0c8f
Neven Mitrovic <153770602+nevenMitrovic@users.noreply.github.com>
2024-11-13 10:47:28 +0100

    postavka tailwindcss i boje, font jos uvek ne radi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        custom: ['Jaldi', 'sans-serif'],
+      },
+      screens: {
+        sm: '481px',
+        // => @media (min-width: 320px) { ... }
+        md: '745px',
+        // => @media (min-width: 744px) { ... }
+        lg: '1025px',
+        // => @media (min-width: 1024px) { ... }
+        xl: '1441px',
+        // => @media (min-width: 1440px) { ... }
+        xxl: '1921px',
+        // => @media (min-width: 1920px) { ... }
+      },
+      colors: {
+        primary: '#FAB12F',
+        secondary: '#FA812F',
+        accent: '#FA4032',
+        background: '#FEF3E2',
+      },
+    },
   },
   plugins: [],
 }
-
